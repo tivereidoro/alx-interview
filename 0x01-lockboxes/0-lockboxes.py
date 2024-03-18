72b47d9521ff9694f49dea3befc83e075e8bdd5c
tivereidoro <tivereidoro@yahoo.com>
2024-03-18 01:53:02 +0100

    Solved

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module 0-lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    There are n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1,
+    and each box may contain keys to the other boxes.
+    Determine if all the boxes can be opened.
+    """
+    position = 0
+    unlocked = {}
+
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+        
+    return False
